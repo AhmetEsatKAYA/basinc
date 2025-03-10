c028036c6ac5238bd8ff3bcbcfe9f141bb959630
Ahmet Esat KAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2025-03-10 14:52:55 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+const questions = [
+    { image: "question1.jpg", answers: ["A", "B", "C", "D"], correct: "B" },
+    { image: "question2.jpg", answers: ["A", "B", "C", "D"], correct: "C" },
+    { image: "question3.jpg", answers: ["A", "B", "C", "D"], correct: "A" },
+    { image: "question4.jpg", answers: ["A", "B", "C", "D"], correct: "D" },
+    { image: "question5.jpg", answers: ["A", "B", "C", "D"], correct: "C" },
+    { image: "question6.jpg", answers: ["A", "B", "C", "D"], correct: "B" },
+    { image: "question7.jpg", answers: ["A", "B", "C", "D"], correct: "A" },
+    { image: "question8.jpg", answers: ["A", "B", "C", "D"], correct: "D" },
+    { image: "question9.jpg", answers: ["A", "B", "C", "D"], correct: "C" },
+    { image: "question10.jpg", answers: ["A", "B", "C", "D"], correct: "A" },
+    { image: "question11.jpg", answers: ["A", "B", "C", "D"], correct: "B" },
+    { image: "question12.jpg", answers: ["A", "B", "C", "D"], correct: "D" },
+    { image: "question13.jpg", answers: ["A", "B", "C", "D"], correct: "C" },
+    { image: "question14.jpg", answers: ["A", "B", "C", "D"], correct: "A" },
+    { image: "question15.jpg", answers: ["A", "B", "C", "D"], correct: "B" }
+];
